2cdaa71774bd99c9672cd90b4a85532305fc4182
justanotherbyte <wishymovies@gmail.com>
2024-12-01 11:05:38 +0000

    Update add_day.py script to include C++ setup

--- add_day.py
+++ add_day.py
@@ -4,7 +4,7 @@ import sys
 import requests
 
 
-DAY_TEMPLATE = """with open('{}/input.txt', 'r') as f:
+PY_TEMPLATE = """with open('{}/input.txt', 'r') as f:
     lines = f.readlines()
 
 def part1():
@@ -13,31 +13,69 @@ def part1():
 def part2():
     ...
 
-print('Part 1:', part1())
-print('Part 2:', part2())
+print("Part 1:", part1())
+print("Part 2:", part2())
 """
+
+CPP_TEMPLATE = """#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int part1() {
+    ifstream f("day01/input.txt");
+    
+    string line;
+    while (getline(f, line)) {
+        // do something...
+    }
+}
+
+int part2() {
+    ifstream f("day01/input.txt");
+    
+    string line;
+    while (getline(f, line)) {
+        // do something...
+    }
+}
+
+int main() {
+    cout << "Part 1: " << part1() << endl;
+    cout << "Part 2: " << part2() << endl;
+}
+"""
+
 
 def download(day: str) -> bytes:
     with open(".session", "r") as f:
         session = f.read().strip()
 
     cookies = {"session": session}
-    resp = requests.get(f"https://adventofcode.com/2024/day/{day}/input", cookies=cookies)
+    resp = requests.get(
+        f"https://adventofcode.com/2024/day/{day}/input", cookies=cookies)
     resp.raise_for_status()
 
     return resp.content
 
+
 def setup(day: str) -> None:
-    dirname = f"day{day}"
+    puzzle_input = download(day)
+    dirname = "day{:02}".format(int(day))
     os.mkdir(dirname)
     os.chdir(dirname)
 
     with open("solution.py", "w") as f:
-        f.write(DAY_TEMPLATE.format(dirname))
+        f.write(PY_TEMPLATE.format(dirname))
+        
+    with open("solution.cpp", "w") as f:
+        f.write(CPP_TEMPLATE.replace("<DAY_DIR>", dirname))
 
-    problem_input = b"TEST"
     with open("input.txt", "wb") as f:
-        f.write(problem_input) # type: ignore
+        f.write(puzzle_input)  # type: ignore
+
 
 day = sys.argv[-1]
 setup(day)
+print("Setup complete...")
